f90f26cb9849ce5162789b76c50d6176a4ba180a
diego <diego_6525@hotmail.com>
2018-06-13 22:24:13 -0700

    need to pull previous work

--- Aggregate.java
+++ Aggregate.java
@@ -1,12 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.*;
 public class Aggregate {
     public static void main (String [] args){
         try {
-            String function = args[0];
-            System.out.println (function);
-            String column = args[1];
-            System.out.println (column);
-            String in = args [2];
-            System.out.println (in);
+            String [] parameters = new String [3];
+            parameters[0] = args[0];
+            parameters[1] = args[1];
+            parameters[2] = args [2];
             if (args.length > 3){
                 int len = args.length - 3;
                 String [] groups = new String [len];
@@ -19,8 +20,18 @@ public class Aggregate {
             }
 
         }catch (Exception e){
-            System.out.println ("There's no function!!!");
+            System.err.println ("There's no function!!!");
         }
+        organize(parameters, groups);
+    }
 
+    public void organize (String [] parameters, String[] groups){
+        try {
+            br = new BufferedReader(parameters[1]);
+        }catch (IOException e){
+            System.err.print("Error reading file \n" + parameters[1]);
+        }
+        ArrayList <String> list = new ArrayList<>();
+        // only get what we need
     }
 }
